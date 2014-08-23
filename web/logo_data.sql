eb34f4177bc55fb275ea83bcf90d23ccb0a757d1
Ben Simonsen <bsimonse@vt.edu>
2014-08-23 15:29:22 -0400

    Server Backup made

---
+++ web/logo_data.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 23, 2014 at 09:28 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `logo_data`
+--
+CREATE DATABASE IF NOT EXISTS `logo_data` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `logo_data`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `battles`
+--
+
+CREATE TABLE IF NOT EXISTS `battles` (
+  `company_id` int(11) NOT NULL,
+  `disgust_wins` int(11) DEFAULT '0',
+  `disgust_losses` int(11) DEFAULT '0',
+  `happiness_wins` int(11) DEFAULT '0',
+  `happiness_losses` int(11) DEFAULT '0',
+  `anger_wins` int(11) DEFAULT '0',
+  `anger_losses` int(11) DEFAULT '0',
+  `excitement_wins` int(11) DEFAULT '0',
+  `excitement_losses` int(11) DEFAULT '0',
+  `desire_wins` int(11) DEFAULT '0',
+  `desire_losses` int(11) DEFAULT '0',
+  `fear_wins` int(11) DEFAULT '0',
+  `fear_losses` int(11) DEFAULT '0',
+  `power_wins` int(11) DEFAULT '0',
+  `power_losses` int(11) DEFAULT '0',
+  `calmness_wins` int(11) DEFAULT '0',
+  `calmness_losses` int(11) DEFAULT '0',
+  `safety_wins` int(11) DEFAULT '0',
+  `safety_losses` int(11) DEFAULT '0',
+  PRIMARY KEY (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `battles`
+--
+
+INSERT INTO `battles` (`company_id`, `disgust_wins`, `disgust_losses`, `happiness_wins`, `happiness_losses`, `anger_wins`, `anger_losses`, `excitement_wins`, `excitement_losses`, `desire_wins`, `desire_losses`, `fear_wins`, `fear_losses`, `power_wins`, `power_losses`, `calmness_wins`, `calmness_losses`, `safety_wins`, `safety_losses`) VALUES
+(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(26, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(31, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(33, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(35, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(39, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(46, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(49, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE IF NOT EXISTS `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `industry` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `company_name` (`company_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=50 ;
+
+--
+-- Dumping data for table `companies`
+--
+
+INSERT INTO `companies` (`id`, `company_name`, `industry`) VALUES
+(1, 'Nike', NULL),
+(2, 'Nissan', NULL),
+(3, 'McDonald''s', NULL),
+(4, 'Intel', NULL),
+(5, 'Burger King', NULL),
+(6, 'Wendy''s', NULL),
+(7, 'Arby''s', NULL),
+(8, 'Sparc', NULL),
+(9, 'Mountain Dew', NULL),
+(10, 'Samsung', NULL),
+(11, 'Google', NULL),
+(12, 'Apple', NULL),
+(13, 'Microsoft', NULL),
+(14, 'Adobe', NULL),
+(15, 'Ford', NULL),
+(16, 'Chevrolet', NULL),
+(17, 'The Pirate Bay', NULL),
+(18, 'Budweiser', NULL),
+(19, 'Coca-Cola', NULL),
+(20, 'Red Bull', NULL),
+(21, 'Starbucks', NULL),
+(22, 'Toyota', NULL),
+(23, 'Asus', NULL),
+(24, 'Nabisco', NULL),
+(25, 'Keebler', NULL),
+(26, 'Nestle', NULL),
+(27, 'Umbrella', NULL),
+(28, 'EA', NULL),
+(29, 'Blizzard', NULL),
+(30, 'Black Mesa', NULL),
+(31, 'Aperture Laboratories', NULL),
+(32, 'Walmart', NULL),
+(33, 'Target', NULL),
+(34, 'Facebook', NULL),
+(35, 'Symantec', NULL),
+(36, 'Twitter', NULL),
+(37, 'Lego', NULL),
+(38, 'Nintendo', NULL),
+(39, 'Sony', NULL),
+(40, 'Verizon', NULL),
+(41, 'AT', NULL),
+(42, 'Comcast', NULL),
+(43, 'Tesla', NULL),
+(44, 'Pabst Blue Ribbon', NULL),
+(45, 'Github', NULL),
+(46, 'Dominos', NULL),
+(47, 'Disney', NULL),
+(48, 'NSA', NULL),
+(49, 'Steam', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `industries`
+--
+
+CREATE TABLE IF NOT EXISTS `industries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `industry_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
